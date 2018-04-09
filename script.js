f0c99a46a66835a84fa21006250eb1a763a42c2a
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-04-09 14:12:10 +0200

    maintenant le css du hover est fait en j;2C;2Cava

--- script.js
+++ script.js
@@ -23,7 +23,15 @@ $(document).ready(function(){
                 }
 
             }
-        })
+        });
+        $(".button").hover(
+            function(){
+                $(this).addClass("hovered");
+            },
+            function(){
+                $(this).removeClass("hovered");
+            }
+        );
     },
     validIndex = function (indexToCheck){
         return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
